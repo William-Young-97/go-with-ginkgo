3ae230f48c133d49d218c050fd5a8d115d235693
EngineerBetter <eb@EngineerBetters-MacBook-Pro.local>
2022-04-05 17:20:40 +0100

    Trying to get package to test

--- testing/test.go
+++ testing/test.go
@@ -1,1 +1,9 @@
 package testing
+
+type Person struct {
+	Age int
+}
+
+func (p *Person) isChild() bool {
+	return p.Age < 18
+}
--- testing/testing_suite_test.go
+++ testing/testing_suite_test.go
@@ -1,13 +1,24 @@
 package testing_test
 
 import (
+	testing2 "simple-test/testing"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-func TestTesting(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Testing Suite")
+func TestTesting(t *testing.T) { // This is the entry point for Ginkgo
+	RegisterFailHandler(Fail)    // The single line of glue code connecting Ginkgo to Gomega
+	RunSpecs(t, "Testing Suite") // Tells Ginkgo to start the test suite
 }
+
+var _ = Describe("Person.IsChild()", func() {
+	Context("when person is a child", func() {
+		It("returns true", func() {
+			person := testing2.Person{Age: 10}
+			response := person.IsChild()
+			Expect(response).To(BeTrue())
+		})
+	})
+})
